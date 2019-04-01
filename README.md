c8503a250b4b076537ef2908f42e0c69d0323715
joshua-miles <joshua@christetulit.org>
2019-04-01 14:46:55 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Moveable Character Challenge
+
+We're going to make our character move on it's own from within the chrome console.
--- index.js
+++ index.js
@@ -1,31 +1,54 @@
- // Set up a variable to use when we need to set the characters image src
- const ASSET_ROOT = './assets/green-character'
+// Set up a variable to use when we need to set the characters image src
+const ASSET_ROOT = './assets/green-character'
 
- // Create an img element to represent the character
- const character = document.createElement('img')
+// Create an img element to represent the character
+const character = document.createElement('img')
 
- // Make the character a little bigger
- character.style.width = '75px'
+// Make the character a little bigger
+character.style.width = '75px'
 
- // Set the src of the img so the browser knows what to show
- character.src = `${ASSET_ROOT}/static.gif`
+// Position the character absolutely in the lower left corner of the screen
+character.style.position = 'absolute'
+character.style.left = '0px'
+character.style.bottom = '0px'
 
- // Add the character image to the page
- document.body.append(character)
+// Set the src of the img so the browser knows what to show
+character.src = `${ASSET_ROOT}/static.gif`
 
+// Add the character image to the page
+document.body.append(character)
 
- function walkLeft(){
+let direction = null
 
- }
+setInterval(function(){
 
- function walkRight(){
+    const left = parseInt(character.style.left)
+    const bottom = parseInt(character.style.bottom)
 
- }
+    if(direction == 'right'){
+        character.style.left = `${left+5}px`
+    }
 
- function walkUp(){
+}, 20)
 
- }
+function walkLeft(){
 
- function walkDown(){
+}
 
- }
+function walkRight(){
+    character.src = `${ASSET_ROOT}/walkright.gif`
+    direction = 'right'
+}
+
+function walkUp(){
+
+}
+
+function walkDown(){
+
+}
+
+function stop(){
+    character.src = `${ASSET_ROOT}/static.gif`
+    direction = null
+}
